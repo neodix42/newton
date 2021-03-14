8e8189679a054485c593c311ab57413859bd8bf0
AleksejMelman <52255577+AleksejMelman@users.noreply.github.com>
2021-03-14 12:07:22 +0200

    Update build_mac.sh

--- build_mac.sh
+++ build_mac.sh
@@ -10,7 +10,7 @@ sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
 
 git clone --recursive https://github.com/newton-blockchain/wallet-desktop.git
 
-cd Wallet\ThirdParty\rlottie
+cd Wallet/ThirdParty/rlottie
 
 git fetch
 git checkout master
